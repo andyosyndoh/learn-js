3dee24755c8b0838ae5aef66ad33e25343fb6a31
aosindo <andyovvo8@gmail.com>
2024-08-26 13:54:09 +0300

    Added declarations.js

--- declarations.js
+++ declarations.js
@@ -1,4 +1,4 @@
 const escapeStr = "`\\\/\"'";
-const arr = [4,undefined, '2'];
+const arr = [4,'2'];
 const obj = {str : "hello",num : 3, bool: true, undef: undefined};
-const nested = {arr: [4,2], obj : {str : "hello", num:5, bool: true}};
+const nested = {arr: [4, undefined, '2'], obj : {str : "hello", num:5, bool: true}};
