19d42b2a31d24d4c1e1ef403f9b9667432404f7d
aosindo <andyovvo8@gmail.com>
2024-08-26 14:30:23 +0300

    Added mutability.js

---
+++ mutability.js
@@ -0,0 +1,32 @@
+const person = {
+    name: 'Rick',
+    age: 78,
+    country: 'FR',
+}
+
+const clone1 = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+}
+
+const clone2 = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+}
+
+const samePerson = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+}
+
+Object.freeze(clone1);
+Object.freeze(clone2);
+
+samePerson.name = person.name;
+samePerson.age = person.age;
+samePerson.country = person.name;
+
+
