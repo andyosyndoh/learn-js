b346b68623a591794e61590cec739d602d36d807
aosindo <andyovvo8@gmail.com>
2024-08-27 13:21:29 +0300

    modified physics.js

--- physics.js
+++ physics.js
@@ -1,3 +1,18 @@
-function getAcceleration(f,m) {
-    return f/m;
+const obj = {
+    f: 10,
+    m: 5,
+    Δv: 100,
+    Δt: 50,
+    t: 1,
+    d: 10
+}
+
+function getAcceleration(obj) {
+    if (obj === null) {
+        return "impossible";
+    }
+    a = obj.f / obj.Δvm;
+    a = obj.Δv / obj.Δt;
+    a = 2*obj.d / obj.t ^ 2;
+    return a;
 };
