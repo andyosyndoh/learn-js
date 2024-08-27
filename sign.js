98ab577e9f376bf7d3676a5aabe00780b23c27fd
aosindo <andyovvo8@gmail.com>
2024-08-27 11:55:21 +0300

    added sign

---
+++ sign.js
@@ -0,0 +1,19 @@
+function sign(num) {
+    if (num === 0) {
+        return 0;
+    };
+    if (num > 0 ) {
+        return 1;
+    };
+    return -1;
+};
+
+function sameSign(num1,num2) {
+    if (num1 < 0 && num2 < 0) {
+        return true;
+    };
+    if (num1 > 0 && num2 > 0) {
+        return true;
+    };
+    return false;
+}
