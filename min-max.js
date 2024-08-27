8117efb694be1036f6d433b61b39790fc6e85a14
aosindo <andyovvo8@gmail.com>
2024-08-27 11:48:56 +0300

    added min max

---
+++ min-max.js
@@ -0,0 +1,13 @@
+function max(num1, num2) {
+    if (num2 > num1) {
+        return num2;
+    };
+    return num1;
+};
+
+function min(num1, num2) {
+    if (num2 < num1) {
+        return num2;
+    };
+    return num1;
+};
