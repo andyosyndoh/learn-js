f01ec115495bfe6731f93dec4393aaa8584b8e73
aosindo <andyovvo8@gmail.com>
2024-08-28 12:18:16 +0300

    Changed multiply

--- elementary.js
+++ elementary.js
@@ -1,11 +1,18 @@
-function multiply(a,b) {
-    return a*b;
+function multiply(a, b) {
+    if (b === 1) {
+        return a;
+    };
+
+    for (let i = 1; i < b; i++) {
+        a += a;
+    };
+    return a
 };
 
-function divide(a,b) {
-    return a/b;
+function divide(a, b) {
+    return a / b;
 };
 
-function modulo(a,b) {
-    return a%b;
+function modulo(a, b) {
+    return a % b;
 };
