1dc4eb7b87365651f00c2305d6f3ee90ceeb3c87
aosindo <andyovvo8@gmail.com>
2024-08-28 14:52:59 +0300

    modified index

--- index-of.js
+++ index-of.js
@@ -14,9 +14,9 @@ function indexOf(arr, n, start) {
 
 function lastIndexOf(arr, n, start) {
     let begin = 0;
-    if (start != undefined) {
-        begin = start;
-    };
+    // if (start != undefined) {
+    //     begin = start;
+    // };
     let x = undefined;
     const length = arr.length
     for (let i = begin; i < length; i++) {
@@ -45,4 +45,4 @@ function includes(arr, n, start) {
 };
 
 console.log(indexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2));
-
+console.log(lastIndexOf([t, 0, 0, t], t, 2))
