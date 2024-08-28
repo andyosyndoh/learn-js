4b022fc406dd972484e5ec2c6f0231e366fc5a45
aosindo <andyovvo8@gmail.com>
2024-08-28 12:26:06 +0300

    modified multiply

--- elementary.js
+++ elementary.js
@@ -2,10 +2,22 @@ function multiply(a, b) {
     if (b === 1) {
         return a;
     };
+    let is = false;
+    if (a < 0){
+        a = -a;
+        is = true;
+    };
+    if (b < 0){
+        b = -b;
+        is = true;
+    };
     let ans = 0
     for (let i = 0; i < b; i++) {
         ans += a;
     };
+    if (is) {
+        return -ans
+    }
     return ans
 };
 
