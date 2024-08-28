dc91ee78ff60fc3b28ebc8af993dfb366a05f2df
aosindo <andyovvo8@gmail.com>
2024-08-28 17:56:58 +0300

    modified index

--- index-of.js
+++ index-of.js
@@ -17,7 +17,7 @@ function indexOf(arr, n, start) {
 };
 
 function lastIndexOf(arr, n, start) {
-    start = arr.length-start
+    start = arr.length-start-1
     for (let i = start; i >= 0; i--) {
         if (arr[i] === n) {
             return i;
