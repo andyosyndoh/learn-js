dc9a06e32cfecbda7b0b1dc89ed6821b7f673610
aosindo <andyovvo8@gmail.com>
2024-08-28 18:28:57 +0300

    modified index

--- slicer.js
+++ slicer.js
@@ -1,13 +1,17 @@
-function slice(arr, start, end){
+function slice(arr, start, end) {
     let finish = arr.length;
-    if (end !== undefined){
+    if (end !== undefined) {
         finish = end;
     };
+    if (start < 0 && end === undefined) {
+        start = arr.length + start;
+        finish = arr.length;
+    }
     let result = '';
-    for (let i= start;i < finish;i++){
+    for (let i = start; i < finish; i++) {
         result += arr[i]
     };
     return result;
 };
 
-console.log(slice('abcdef', 2));
+console.log(slice('abcdef', -2));
