754900b9ec0e65f869d4222d307c5ae3e7fc965f
aosindo <andyovvo8@gmail.com>
2024-08-28 14:06:07 +0300

    Added indexof.js

---
+++ index-of.js
@@ -0,0 +1,46 @@
+function indexOf(arr, n,start) {
+    let begin = 0;
+    if (start != undefined) {
+        begin = start;
+    };
+    const length = arr.length()
+    for (i = begin;i < length;i++){
+        if (arr[i] === n){
+            return i;
+        };
+    };
+    return -1;
+};
+
+function lastIndexOf(arr, n,start) {
+    let begin = 0;
+    if (start != undefined) {
+        begin = start;
+    };
+    let x = undefined;
+    const length = arr.length()
+    for (i = begin;i < length;i++){
+        if (arr[i] === n){
+            x = i;
+        };
+    };
+    if (x === undefined) {
+        return -1;
+    };
+    return x;
+};
+
+function indexOf(arr, n,start) {
+    let begin = 0;
+    if (start != undefined) {
+        begin = start;
+    };
+    const length = arr.length()
+    for (i = begin;i < length;i++){
+        if (arr[i] === n){
+            return true;
+        };
+    };
+    return false;
+};
+
