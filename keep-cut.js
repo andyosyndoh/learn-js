b3552e888857edc0972dea3677814aa71e601e06
aosindo <andyovvo8@gmail.com>
2024-08-29 13:16:57 +0300

    added keep

---
+++ keep-cut.js
@@ -0,0 +1,16 @@
+function cutFirst(str) {
+    return slice(str, 2);
+};
+
+function cutLast(str) {
+    return slice(str, -2);
+};
+
+function cutFirstLast(str) {
+    const length = str.length-2; 
+    return slice(str, 2, 2);
+};
+
+function KeepFirst(str) {
+    return slice(str, 0, );
+};
