af4b0cf48111c668ccd60f6eb3cd50c9b9f5cd58
aosindo <andyovvo8@gmail.com>
2024-08-29 16:37:34 +0300

    modified cut conner

--- cut-corners.js
+++ cut-corners.js
@@ -1,17 +1,48 @@
 function round(num) {
-    let res = [];
-    for (let i = 0; i < num.length; i++) {
-        let first = Math.floor(num[i]);
-        if ((num[i] - first) >= 0.5) {
+    // let res = [];
+    // for (let i = 0; i < num.length; i++) {
+        let first = Math.floor(num);
+        if ((num - first) >= 0.5) {
             let ans = first + 1;
             res.push(ans);
         } else {
             res.push(first);
         };
 
-    };
+    // };
+    // return res;
+};
+
+function ceil(num) {
+    // let res = [];
+    // for (let i = 0; i < num.length; i++) {
+        let first = Math.ceil(num[i]);
+        res.push(first);
+    // };
     return res;
 };
 
-// const nums = [3, -3, 3, -3, 0];
-// console.log(round(nums));
+function floor(num) {
+    // let res = [];
+    // for (let i = 0; i < num.length; i++) {
+        let first = Math.floor(num[i]);
+        res.push(first);
+    // };
+    return res;
+};
+
+function trunc(num) {
+    // let res = [];
+    // for (let i = 0; i < num.length; i++) {
+        let first = Math.trunc(num[i]);
+        res.push(first);
+    // };
+    return res;
+};
+
+
+// const nums = [3.7, -3.7, 3.1, -3.1];
+// console.log((round(nums)))
+// console.log((floor(nums)))
+// console.log((trunc(nums)))
+// console.log((ceil(nums)))
