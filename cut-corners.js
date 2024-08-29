f86c0faddc2d83941cdf6f05d83885b7170c1987
aosindo <andyovvo8@gmail.com>
2024-08-29 15:41:18 +0300

    modified cut coners

--- cut-corners.js
+++ cut-corners.js
@@ -1,6 +1,7 @@
 function round(num) {
-    let first = Math.floor(num);
-
-    if ((num - first) > 0.5) first += 1;
-    return first;
+    for (let i = 0; i < num.length; i++) {
+        let first = Math.floor(num[i]);
+        if ((num[i] - first) > 0.5) first += 1;
+        return first;
+    };
 };
