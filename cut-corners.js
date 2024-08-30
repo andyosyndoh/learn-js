f0ceff3e8ba24783d26f4bfa17a05740d6da4bb7
aosindo <andyovvo8@gmail.com>
2024-08-30 13:56:21 +0300

    modified cut coners

--- cut-corners.js
+++ cut-corners.js
@@ -117,7 +117,7 @@ function floor(num) {
     let rem = modulo(num, 1);
     let whole = num - rem;
     if (neg === true) {
-        return whole * -1;
+        return (whole * -1);
     } else {
         return whole;
     };
