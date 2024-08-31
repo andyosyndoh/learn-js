2f9791e9683dbaeb8780e590ceb9dbff8ef5ec20
aosindo <andyovvo8@gmail.com>
2024-08-31 10:58:50 +0300

    modified vowels variable

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,10 +1,10 @@
-function vowelDots(str) {
-    let vowels = /[aeiouAEIOU]/g;
+let vowels = /[aeiouAEIOU]/g;
 
+function vowelDots(str) {
     return str.replace(vowels, '$&.');
 }
 
-// console.log(vowelDots('hello')); // Output: "he.llo."
-// console.log(vowelDots('world')); // Output: "wo.rld"
-// console.log(vowelDots('AEIOU')); // Output: "A.E.I.O.U."
-// console.log(vowelDots('JavaScript')); // Output: "Ja.va.Sc.ri.pt"
+console.log(vowelDots('hello')); // Output: "he.llo."
+console.log(vowelDots('world')); // Output: "wo.rld"
+console.log(vowelDots('AEIOU')); // Output: "A.E.I.O.U."
+console.log(vowelDots('JavaScript')); // Output: "Ja.va.Sc.ri.pt"
