04e91e07c202859d0c941b8f811edcf7e62d6d58
aosindo <andyovvo8@gmail.com>
2024-08-31 12:04:41 +0300

    modified samedata

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,13 @@
 function sameAmount(str, regex1, regex2) {
-    const matches1 = str.match(regex1) || [];
-    const matches2 = str.match(regex2) || [];
+    const matches1 = str.match(regex1);
+    const matches2 = str.match(regex2);
 
-    return matches1.length === matches2.length;
+    if (!matches1 && !matches2) {
+        return true;
+    }
+
+    return (matches1 ? matches1.length : 0) === (matches2 ? matches2.length : 0);
 }
+
+const data = "some string without qqqqqqq";
+console.log(sameAmount(data, /q /, /qqqqqqq/)); // Output: true
