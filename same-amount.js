41e076424cbbe430a749e8f55a12de3fdd9d5926
aosindo <andyovvo8@gmail.com>
2024-08-31 12:43:46 +0300

    modified same amount

--- same-amount.js
+++ same-amount.js
@@ -1,12 +1,12 @@
 function sameAmount(str, regex1, regex2) {
-    const matches1 = str.match(regex1);
-    const matches2 = str.match(regex2);
+    const matches1 = str.match(regex1) || [];
+    const matches2 = str.match(regex2) || [];
 
-    if (!matches1 && !matches2) {
+    if (matches1 === matches2) {
         return true;
     }
 
-    return (matches1 ? matches1.length : 0) === (matches2 ? matches2.length : 0);
+    return (matches1.length) === (matches2.length);
 }
 
 // const data = "some string without qqqqqqq";
