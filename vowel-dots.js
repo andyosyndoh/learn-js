65c54db40b4c5e86e1d0a93acd8e82c37d12560c
aosindo <andyovvo8@gmail.com>
2024-08-31 10:53:34 +0300

    added voweldots

---
+++ vowel-dots.js
@@ -0,0 +1,10 @@
+function vowelDots(str) {
+    const vowels = /[aeiouAEIOU]/g;
+
+    return str.replace(vowels, '$&.');
+}
+
+console.log(vowelDots('hello')); // Output: "he.llo."
+console.log(vowelDots('world')); // Output: "wo.rld"
+console.log(vowelDots('AEIOU')); // Output: "A.E.I.O.U."
+console.log(vowelDots('JavaScript')); // Output: "Ja.va.Sc.ri.pt"
