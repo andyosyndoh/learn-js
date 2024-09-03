d2295302c14a0ac4014424c3f84b4d233088fa6a
aosindo <andyovvo8@gmail.com>
2024-09-03 10:09:11 +0300

    modified date for testing

--- date-is.js
+++ date-is.js
@@ -1,30 +1,30 @@
-function isValid(date){
+function isValid(date) {
     let result = new Date(date);
     if (result == 'Invalid Date') return false;
     return true
 }
 
-function isAfter(date1,date2){
+function isAfter(date1, date2) {
     if (date1 > date2) return true;
     return false;
 }
 
-function isBefore(date1,date2){
+function isBefore(date1, date2) {
     if (date1 < date2) return true;
     return false;
 }
 
-function isFuture(date){
+function isFuture(date) {
     const currentDate = new Date();
-    if (isValid(date)&& currentDate > date) return true;
+    if (isValid(date) && currentDate > date) return true;
     return false;
 }
 
-function isPast(date){
-    const currentDate = new Date();
-    if (isValid(date)&& currentDate < date) return true;
-    return false;
-}
+// function isPast(date) {
+//     const currentDate = new Date();
+//     if (isValid(date) && currentDate < date) return true;
+//     return false;
+// }
 
-console.log(isValid('2016-01-01'));
+console.log(isValid('2013-01-01'));
 console.log(isValid(''));
