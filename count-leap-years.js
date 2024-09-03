6491d0112193d11cd4a1ea8db1199b232a24a23a
aosindo <andyovvo8@gmail.com>
2024-09-03 15:23:09 +0300

    added count leap years

---
+++ count-leap-years.js
@@ -0,0 +1,10 @@
+function countLeapYears(date) {
+    let year = date.getyear()
+    let count = 0;
+    for (let i = 0; i < year; i++) {
+        if (i%4 === 0){
+            count++
+        }
+    }
+    return count
+}
