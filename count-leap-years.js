775e8bb70e33d2b6bbf3d4676bef6e4aff264e53
aosindo <andyovvo8@gmail.com>
2024-09-03 15:42:23 +0300

    modified count leap years

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,23 +1,23 @@
-function countLeapYears(date) {
-    const year = date.getFullYear();
-    let count = 0;
-    for (let i = 0; i < year; i++) {
-        if (i%4 === 0){
-            count++
-        }
-    }
-    return count
-}
-
 // function countLeapYears(date) {
 //     const year = date.getFullYear();
-
-//     function countLeapYearsUpTo(year) {
-//         return Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400);
+//     let count = 0;
+//     for (let i = 0; i < year; i++) {
+//         if (i%4 === 0){
+//             count++
+//         }
 //     }
-
-//     return countLeapYearsUpTo(year);
+//     return count
 // }
+
+function countLeapYears(date) {
+    const year = date.getFullYear();
+
+    function countLeapYearsUpTo(year) {
+        return Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400);
+    }
+
+    return countLeapYearsUpTo(year);
+}
 
 // Convert the timestamp to a Date object
 // console.log(countLeapYears(new Date(Date.now())));
