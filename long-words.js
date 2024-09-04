36f0f969b408383bbf0c4d92fdb213bcd27f75ea
aosindo <andyovvo8@gmail.com>
2024-09-04 12:04:25 +0300

    added longwords

---
+++ long-words.js
@@ -0,0 +1,13 @@
+function longWords(arr) {
+    const match = arr.filter(obj => typeof obj === 'string' && obj.length >= 5) || [];
+    return match.length === arr.length;
+}
+
+function longWords(arr) {
+    const match = arr.filter(obj => typeof obj === 'string' && obj.length >= 10) || [];
+    return match.length > 0;
+}
+
+function noLongWords(arr) {
+    return !arr.some(obj => typeof obj === 'string' && obj.length >= 7);
+}
