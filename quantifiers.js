9634cc2ca8c93359e6a1a429b5cefe32c3e9da13
aosindo <andyovvo8@gmail.com>
2024-09-04 13:44:00 +0300

    added quantifier

---
+++ quantifiers.js
@@ -0,0 +1,21 @@
+function every(array, func) {
+    for (let i = 0; i < array.length; i++) {
+        if (!func(array[i], i,array)){
+            return false
+        }
+    }
+    return true
+}
+
+function some(array, func) {
+    for (let i = 0; i < array.length; i++) {
+        if (func(array[i], i,array)){
+            return true
+        }
+    }
+    return false
+}
+
+function none(array, func) {
+   return !some(array,func)
+}
