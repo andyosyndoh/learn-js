b140401bec3be264a5040f7ff52d1d1164ed5b54
aosindo <andyovvo8@gmail.com>
2024-09-09 14:22:15 +0300

    added invert

---
+++ invert.js
@@ -0,0 +1,11 @@
+function invert(obj) {
+    const invertedObj = {};
+
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            invertedObj[obj[key]] = key;
+        }
+    }
+
+    return invertedObj;
+}
