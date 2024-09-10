7052c557dea6e86d9410905955fe8e590d50c947
aosindo <andyovvo8@gmail.com>
2024-09-10 14:12:25 +0300

    added manipulate keys

---
+++ manipulate-keys.js
@@ -0,0 +1,27 @@
+function filterKeys(obj, predicate) {
+    return Object.keys(obj)
+        .filter(predicate)
+        .reduce((res, key) => {
+            res[key] = obj[key];
+            return res;
+        }, {});
+}
+
+function mapKeys(obj, callback) {
+    return Object.keys(obj)
+        .map(callback)
+        .reduce((res, key, i) => {
+            res[key] = obj[Object.keys(obj)[i]];
+            return res;
+        }, {});
+}
+
+function reduceKeys(obj, callback, initialValue) {
+    if (initialValue === undefined) {
+        initialValue = "";
+    }
+    let res = Object.keys(obj).reduce((acc, curr) => {
+        return callback(acc, curr, initialValue);
+    }, initialValue);
+    return res;
+}
