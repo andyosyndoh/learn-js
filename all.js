84876f00c2ed485abce3fc08ae715c4ff0a3856b
aosindo <andyovvo8@gmail.com>
2024-09-14 20:27:15 +0300

    modified all

--- all.js
+++ all.js
@@ -1,8 +1,8 @@
 async function all(obj = {}) {
     var res = {};
     if (Object.keys(obj).length === 0) return {};
-    for (let key in objs) {
-        res[key] = await objs[key];
+    for (let key in obj) {
+        res[key] = await obj[key];
     }
     return res;
 }
