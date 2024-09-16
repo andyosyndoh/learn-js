ea75cc6bbde11202ff08d2551f58f4468676f577
aosindo <andyovvo8@gmail.com>
2024-09-16 11:47:06 +0300

    modified verydisco

--- verydisco.mjs
+++ verydisco.mjs
@@ -2,7 +2,7 @@ let args = process.argv.slice(2)
 let ans = [];
 for (let i = 0; i < args.length; i++) {
     let len = args[i].length;
-    let half = Math.floor(len / 2);
+    let half = Math.ceil(len / 2);
     let refined = args[i].substring(half) + args[i].substring(0, half)
     ans.push(refined)
 }
