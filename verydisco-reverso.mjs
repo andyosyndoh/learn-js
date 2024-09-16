74789b2960192e3afdd8ee603eca3eaa9fd3e5f2
aosindo <andyovvo8@gmail.com>
2024-09-16 14:56:23 +0300

    added very disco reverse

---
+++ verydisco-reverso.mjs
@@ -0,0 +1,22 @@
+import { readFile } from 'fs/promises';
+
+// Get command line arguments and process them
+const args = process.argv.slice(2);
+const obj = args[0];
+try {
+    const filePath = new URL('./package.json', import.meta.url);
+    const contents = await readFile(filePath, { encoding: 'utf8' });
+    const words = contents.split(' ');
+
+    const ans = words.map(word => {
+        const len = word.length;
+        const half = Math.ceil(len / 2);
+        const refined = word.substring(half) + word.substring(0, half);
+        return refined;
+    });
+
+    const ref = ans.join(' ');
+    console.log(ref)
+} catch (err) {
+    console.error(err.message);
+}
