5251185be2bd398883276edba8aaabebf36469f2
aosindo <andyovvo8@gmail.com>
2024-09-16 14:48:31 +0300

    modified verydisco-forever

--- verydisco-forever.mjs
+++ verydisco-forever.mjs
@@ -1,33 +1,25 @@
-import { writeFile } from 'node:fs/promises';
-import { Buffer } from 'node:buffer';
+const fs = require('fs');
 
+// Get command line arguments and process them
+let args = process.argv.slice(2);
+let obj = args[0];
+args = obj.split(' ');
 
-let args = process.argv.slice(2)
-let obj = args[0]
-args = obj.split(' ')
 let ans = [];
 for (let i = 0; i < args.length; i++) {
     let len = args[i].length;
     let half = Math.ceil(len / 2);
-    let refined = args[i].substring(half) + args[i].substring(0, half)
-    ans.push(refined)
+    let refined = args[i].substring(half) + args[i].substring(0, half);
+    ans.push(refined);
 }
 
-let ref = ans.join(' ')
+let ref = ans.join(' ');
 
-
-try {
-    const controller = new AbortController();
-    const { signal } = controller;
-    const data = new Uint8Array(Buffer.from(ref));
-    const promise = writeFile('verydisco-forever.txt', data, { signal });
-    // Abort the request before the promise settles.
-    controller.abort();
-
-    await promise;
-} catch (err) {
-    // When a request is aborted - err is an AbortError
-    console.error(err);
-}
-
-
+// Write the result to 'verydisco-forever.txt'
+fs.writeFile('verydisco-forever.txt', ref, (err) => {
+    if (err) {
+        console.error('Error writing to file:', err);
+    } else {
+        console.log('File written successfully to verydisco-forever.txt');
+    }
+});
