db2c880a9f0e47496176507dfa0d6abed76a4838
aosindo <andyovvo8@gmail.com>
2024-09-17 14:49:45 +0300

    modified uninvited

--- uninvited.mjs
+++ uninvited.mjs
@@ -32,7 +32,7 @@ const requestListener = async (req, res) => {
                     res.writeHead(201, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify(guestData));
                 } catch (error) {
-                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    res.writeHead(400, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({ error: 'server failed' }));
                 }
             });
